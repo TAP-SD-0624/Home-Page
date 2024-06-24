2115cacd11dec6d7daf8421582722afbb32a0345
amr-imad <amr_imad@hotmail.com>
2024-06-24 17:17:06 +0300

    'fixing'

--- script.js
+++ script.js
@@ -9,86 +9,60 @@ function toggleFavourite(){
     x.style.display='none'
     fav='hidden'
   }
-  
 }
 
-// function changeTheMood(){
-//     const x = document.documentElement.style
-//     if(mood==='day'){
-//         x.setProperty('--bg_default', '#1A1A1A');
-//         x.setProperty('--bg_body', '#282828');
-//         x.setProperty('--lines-color', '#000000');
-//         x.setProperty('--body-text', '#EDEDED');
-//         mood = 'night'
-//     }else{
-//         x.setProperty('--bg_default', '#FFFFFF');
-//         x.setProperty('--bg_body', '#F0F9FF');
-//         x.setProperty('--lines-color', '#DDDDDD');
-//         x.setProperty('--body-text', '#333333');
-//         mood = 'day'
-//     }
-// }
+// favourite toggle
 function changeTheMood(){
   document.getElementById('body').classList.toggle('darkColors')
 }
+//generate Stars used in both files
+function generateStars(parent,rating,starsClass){
+  for(let i=0;i<5;i++){
+    const ratingStar = document.createElement('img')
+    ratingStar.classList.add(starsClass)
+    if(i<Math.round(rating)){
+        ratingStar.src='./assets/icons/stars-filled.svg'
+    }
+    else{
+      ratingStar.src='./assets/icons/stars.svg'
+    }
+    parent.appendChild(ratingStar)
+  }
+}
 
 // extract all the data from the data.js and add them to the main page
-function extractData(x){
-  try {
-    const cardWraper = document.getElementById("cardWraper")
-    x.forEach(element => {
-        const card = document.createElement('div')
-        card.classList.add('card')
-        card.addEventListener('click',()=>goToTopicPage(element.id))
-
-        const cardImage = document.createElement('img')
-        cardImage.classList.add('cardImage')
-        cardImage.src = `./assets/cards/${element.image}`
-        card.appendChild(cardImage)
-
-        const cardInfo = document.createElement('div')
-        cardInfo.classList.add('cardInfo')
-
-        const cardDiscription = document.createElement('p')
-        cardDiscription.classList.add('cardDiscription')
-        cardDiscription.appendChild(document.createTextNode(element.category))
-        cardInfo.appendChild(cardDiscription)
-
-        const cardTitle = document.createElement('h3')
-        cardTitle.classList.add('cardTitle')
-        cardTitle.appendChild(document.createTextNode(element.topic))
-        cardInfo.appendChild(cardTitle)
-
-        const allStars = document.createElement('div')
-        allStars.classList.add('allStars')
-        for(let i=0;i<5;i++){
-            const ratingStars = document.createElement('img')
-            ratingStars.classList.add('ratingStars')
-            if(i<Math.round(element.rating)){
-                ratingStars.src='./assets/icons/stars-filled.svg'
-            }
-            else{
-                ratingStars.src='./assets/icons/stars.svg'
-            }
-            
-            allStars.appendChild(ratingStars)
-        }
-        cardInfo.appendChild(allStars)
-
-
-        const cardAuther = document.createElement('p')
-        cardAuther.classList.add('cardAuther')
-        cardAuther.appendChild(document.createTextNode(element.name))
-        cardInfo.appendChild(cardAuther)
-
-
-        card.appendChild(cardInfo)
-        cardWraper.appendChild(card)
+function extractData(topics){
+        const cardWraper = document.getElementById("cardWraper")
+        topics.forEach(element => {
+          const card = document.createElement('div')
+          card.classList.add('card')
+          card.addEventListener('click',()=>goToTopicPage(element.id))
+          const cardImage = document.createElement('img')
+          cardImage.classList.add('cardImage')
+          cardImage.src = `./assets/cards/${element.image}`
+          card.appendChild(cardImage)
+          const cardInfo = document.createElement('div')
+          cardInfo.classList.add('cardInfo')
+          const cardDiscription = document.createElement('p')
+          cardDiscription.classList.add('cardDiscription')
+          cardDiscription.appendChild(document.createTextNode(element.category))
+          cardInfo.appendChild(cardDiscription)
+          const cardTitle = document.createElement('h3')
+          cardTitle.classList.add('cardTitle')
+          cardTitle.appendChild(document.createTextNode(element.topic))
+          cardInfo.appendChild(cardTitle)
+          const allStars = document.createElement('div')
+          allStars.classList.add('allStars')
+          generateStars(allStars,element.rating,'ratingStars')
+          cardInfo.appendChild(allStars)
+          const cardAuther = document.createElement('p')
+          cardAuther.classList.add('cardAuther')
+          cardAuther.appendChild(document.createTextNode(element.name))
+          cardInfo.appendChild(cardAuther)
+          card.appendChild(cardInfo)
+          cardWraper.appendChild(card)
     })
-  } catch  {
-    
-  }
-    
+
 }
 extractData(data)
 
@@ -98,7 +72,9 @@ extractData(data)
 function getElementById(items,id){
   return items.find(x=>x.id===id)
 }
+// card generator
 function generateMiniCard(imgLink,titleName,rating){
+  
   const miniCard = document.createElement('div')
   miniCard.classList.add('favCard')
   const image = document.createElement('img')
--- scriptTopic.js
+++ scriptTopic.js
@@ -1,60 +1,35 @@
 
-const urlParams = new URLSearchParams(window.location.search);
-const topicId = urlParams.get('topic')
-
+//retrieve id from url
+const topicId = location.search.split('topic=')[1]
 const topic = getElementById(data,parseInt(topicId))
+//generate TopicSubs
+function generateTopicSubs(mainTopic,subtopicsList){
+  document.getElementById('topicSubsHeader').innerText=(`${mainTopic} Sub Topics`)
+  const topicSubs = document.getElementById("topicSubs")
+  subtopicsList.forEach(ele => {
+    const topicSubsUnit = document.createElement('div')
+    topicSubsUnit.classList.add('topicSubsUnit')
+    const topicSubsUnitText = document.createElement('p')
+    topicSubsUnitText.classList.add('topicSubsUnitText')
+    topicSubsUnitText.append(document.createTextNode(ele))
+    const svgCode =  '<svg xmlns="http://www.w3.org/2000/svg" class="checkMark" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M352 176L217.6 336 160 272"/></svg>'
+    topicSubsUnit.innerHTML = svgCode
+    topicSubsUnit.append(topicSubsUnitText)
+    topicSubs.append(topicSubsUnit)
+  });
 
+
+
+}
 function generateTopicPage(element){
-    const topicInfo = document.getElementById('topicInfo')
-
-    const topicTitle = document.createElement('p')
-    topicTitle.classList.add('topicTitle')
-    topicTitle.append(document.createTextNode(element.category))
-    topicInfo.append(topicTitle)
-
-    const topicAbbrev = document.createElement('h2')
-    topicAbbrev.classList.add('topicAbbrev')
-    topicAbbrev.append(document.createTextNode(element.topic))
-    topicInfo.append(topicAbbrev)
-
-    const topicRatingStars = document.createElement('div')
-    for(let i=0;i<5;i++){
-      const ratingStar = document.createElement('img')
-      ratingStar.classList.add('favStar')
-      if(i<Math.round(element.rating)){
-          ratingStar.src='./assets/icons/stars-filled.svg'
-      }
-      else{
-        ratingStar.src='./assets/icons/stars.svg'
-      }
-      
-      topicRatingStars.appendChild(ratingStar)
-    }
-    topicInfo.append(topicRatingStars)
-
-    const topicDescription = document.createElement('p')
-    topicDescription.classList.add('topicDescription')
-    topicDescription.append(document.createTextNode(element.description))
-    topicInfo.append(topicDescription)
-
-    document.getElementById('topicSubsHeader').appendChild(document.createTextNode(`${element.topic} Sub Topics`))
-    const topicSubs = document.getElementById("topicSubs")
-    element.subtopics.forEach(ele => {
-      const topicSubsUnit = document.createElement('div')
-      topicSubsUnit.classList.add('topicSubsUnit')
-
-      const topicSubsUnitText = document.createElement('p')
-      topicSubsUnitText.classList.add('topicSubsUnitText')
-      topicSubsUnitText.append(document.createTextNode(ele))
-      const svgCode =  '<svg xmlns="http://www.w3.org/2000/svg" class="checkMark" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M352 176L217.6 336 160 272"/></svg>'
-      topicSubsUnit.innerHTML = svgCode
-      topicSubsUnit.append(topicSubsUnitText)
-      topicSubs.append(topicSubsUnit)
-    });
-
-
+    document.getElementById('topicTitle').innerText=element.category
+    document.getElementById('topicAbbrev').innerText=element.topic
+    const topicRatingStars = document.getElementById('topicRatingStars')
+    generateStars(topicRatingStars,element.rating,'favStar')
+    generateTopicSubs(element.topic,element.subtopics)
+    document.getElementById('topicDescription').innerText=element.description
     document.getElementById('topicCardImage').src = `./assets/cards/${element.image}`
     document.getElementById('topicCardAttTitle').innerText = element.topic
     document.getElementById('topicCardAttLink').innerText = element.name
-  }
-  generateTopicPage(topic)
+}
+generateTopicPage(topic)
